e543505efd600e24babd5c2e044273506702a287
Isa Costa <isa-hcosta@hotmail.com>
2022-08-18 15:07:40 -0300

    requisito 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p>Conteudo</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
